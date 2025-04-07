235118088b0df2abc203c09562569e083bb84afb
Marya444 <meamea43@gmail.com>
2025-04-08 07:50:24 +0800

    view added genders

--- client/src/services/GenderServices.ts
+++ client/src/services/GenderServices.ts
@@ -1,6 +1,15 @@
 import AxiosInstance from "../AxiosInstance";
 
 const GenderServices = {
+
+  loadGender: async () => {
+    return AxiosInstance.get("/loadGender")
+      .then((response) => response)
+      .catch((error) => {
+        throw error;
+      });
+  },
+
    storeGender: async (data: any) => {
     return AxiosInstance.post("/storeGender", data)
       .then((response) => response)
--- client/src/tables/GenderTable.tsx
+++ client/src/tables/GenderTable.tsx
@@ -1,47 +1,73 @@
-import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
+import Genders from "../interfaces/Genders";
+import GenderServices from "../services/GenderServices";
+import ErrorHandler from "../components/handler/ErrorHandler";
 
 const GenderTable = () => {
+  const [state, setState] = useState({
+    loadingGenders: true,
+    genders: [] as Genders[],
+  });
+
+  const handleLoadGenders = () => {
+    GenderServices.loadGender()
+      .then((res) => {
+        if (res.status === 200) {
+          setState((prevState) => ({
+            ...prevState,
+            genders: res.data.genders,
+          }));
+        } else {
+          console.error(
+            "Unexpected status error during loading genders:",
+            res.status
+          );
+        }
+      })
+
+      .catch((error) => {
+        ErrorHandler(error, null);
+      })
+
+      .finally(() => {
+        setState((prevState) => ({
+          ...prevState,
+          loadingGenders: false,
+        }));
+      });
+  };
+
+  useEffect(() => {
+    handleLoadGenders();
+  }, []);
+
   return (
     <>
       <table className="table table-hover">
         <thead>
-          <tr>
+          <tr className="align-middle">
             <th>No.</th>
             <th>Gender</th>
+            <th>Action</th>
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <td>1</td>
-            <td>Male</td>
-            <td>
-              <div className="btn-group">
-                <Link className="btn btn-success" 
-                >Edit</Link>
-                <Link className="btn btn-danger">Delete</Link>
-              </div>
-            </td>
-          </tr>
-          <tr>
-            <td>2</td>
-            <td>Female</td>
-            <td>
-              <div className="btn-group">
-                <Link className="btn btn-success">Edit</Link>
-                <Link className="btn btn-danger">Delete</Link>
-              </div>
-            </td>
-          </tr>
-          <tr>
-            <td>3</td>
-            <td>Others</td>
-            <td>
-              <div className="btn-group">
-                <Link className="btn btn-success">Edit</Link>
-                <Link className="btn btn-danger">Delete</Link>
-              </div>
-            </td>
-          </tr>
+          {state.genders.map((gender, index) => (
+            <tr className="align-middle">
+              <td>{index + 1}</td>
+              <td>{gender.gender}</td>
+              <td>
+                <div className="btn-group">
+                  <button type="button" className="btn btn-success">
+                    Edit
+                  </button>
+                  <button type="button" className="btn btn-danger">
+                    Delete
+                  </button>
+                </div>
+              </td>
+            </tr>
+          ))}
         </tbody>
       </table>
     </>
